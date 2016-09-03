3b9377dcd31ecc6e5a3db4df492ab096733a8bc1
tavisca-smurkar <smurkar@tavisca.com>
2016-09-03 23:25:28 +0530

    TDD, SOLID Princ. Logging and Exception handling features are added for Draw Circle part

---
+++ DrawShapesOfYouChoice/ShapeForm/CircleForm.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace ShapeForm
+{
+    partial class CircleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.circlePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // circlePanel
+            // 
+            this.circlePanel.Location = new System.Drawing.Point(12, 12);
+            this.circlePanel.Name = "circlePanel";
+            this.circlePanel.Size = new System.Drawing.Size(607, 515);
+            this.circlePanel.TabIndex = 0;
+            // 
+            // CircleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 539);
+            this.Controls.Add(this.circlePanel);
+            this.Name = "CircleForm";
+            this.Text = "CircleForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel circlePanel;
+    }
+}
---
+++ DrawShapesOfYouChoice/ShapeForm/CircleForm.cs
@@ -0,0 +1,38 @@
+﻿using Entities;
+using EntityFactory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShapeForm
+{
+    public partial class CircleForm : Form
+    {
+        Circle circle = CircleFactory.GetCircle();
+        public CircleForm()
+        {
+            InitializeComponent();
+        }
+
+        public CircleForm(Circle circle)
+        {
+            InitializeComponent();
+            this.circle.pointOneXCoordinate = circle.pointOneXCoordinate;
+            this.circle.pointOneYCoordinate = circle.pointOneYCoordinate;
+            this.circle.pointTwoXCoordinate = circle.pointTwoXCoordinate;
+            this.circle.pointTwoYCoordinate = circle.pointTwoYCoordinate;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics graphics = circlePanel.CreateGraphics();
+            Pen pen = new Pen(Color.Red);
+            graphics.DrawEllipse(pen, circle.pointOneXCoordinate, circle.pointOneYCoordinate, circle.pointTwoXCoordinate, circle.pointTwoYCoordinate);
+        }
+    }
+}
